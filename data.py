65ca6ae861656ec53727b94530d464467eba2614
donfiguerres <lyndonfiguerres@gmail.com>
2020-06-14 23:19:11 +0800

    Added module to pull data.
    Former-commit-id: 64923a11ad3b882b2bcab44634edcd6312738b5c [formerly 1798f2d9b40587420ce4ad72160148dd6f298554]
    Former-commit-id: 64923a11ad3b882b2bcab44634edcd6312738b5c
    Former-commit-id: 9c88fae7ea3458c33b4a430c0e15b7eef476b6a7

---
+++ data.py
@@ -0,0 +1,77 @@
+"""
+DOH data drop scraper
+
+The data drop is a bit hard to pull since there is no fixed URL to pull the data
+from. The data drop links are listed in the PDF files that are uploaded daily by
+DOH to Google Drive.
+This module will dowload the latest data to the 'data' directory if ran as a
+script.
+"""
+
+import os
+import sys
+import logging
+import traceback
+
+import pickle
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from apiclient import errors
+
+
+class RemoteFileNotFoundError(Exception):
+    pass
+
+
+def build_gdrive_service():
+    """ This function is derived from the Google Drive API quickstart guide. """
+    creds = None
+    CREDENTIALS_FILE_PATH = "credentials.json"
+    TOKEN_FILE_PATH = "token.pickle"
+    GDRIVE_SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(TOKEN_FILE_PATH):
+        with open(TOKEN_FILE_PATH, 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                                CREDENTIALS_FILE_PATH, GDRIVE_SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(TOKEN_FILE_PATH, 'wb') as token:
+            pickle.dump(creds, token)
+    return build('drive', 'v3', credentials=creds)
+
+
+def get_readme_id(drive_service):
+    DOH_README_FOLDER_ID = '1ZPPcVU4M7T-dtRyUceb0pMAd8ickYf8o'
+    results = drive_service.files().list(
+                q=f"name contains 'READ ME' and parents in '{DOH_README_FOLDER_ID}' and trashed = false",
+                fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+    if not items:
+        raise RemoteFileNotFoundError("DOH Readme Not Found")
+    else:
+        # We expect only one file in the folder.
+        if len(items) > 1:
+            logging.warning("The READ ME contents have changed.")
+        for item in items:
+            return item['id']
+
+def main():
+    drive_service = build_gdrive_service()
+    readme_file_id = get_readme_id(drive_service)
+    return 0
+
+if __name__ == "__main__":
+    try:
+        sys.exit(main())
+    except Exception:
+        logging.error(traceback.format_exc())
+        sys.exit(1)
