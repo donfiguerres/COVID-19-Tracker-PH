ddae5acd47c6a664d86e48457f7400dd0858420b
donfiguerres <donfiguerres@github.com>
2021-07-13 23:37:02 +0800

    Follow PEP 8 guideline of 2 blank lines.
    Former-commit-id: 961953e4253c1b06b17a0da1f0da7694f1d49d72
    Former-commit-id: 495e174a96ee6a076968dc8372ac5b075f8ac7de
    Former-commit-id: b5927ddec263e3454a713c8dd61ce8a6ae85b3d7

--- datadrop.py
+++ datadrop.py
@@ -59,11 +59,13 @@ def build_gdrive_service(credentials_path, token_path, scopes):
             pickle.dump(creds, token)
     return build('drive', 'v3', credentials=creds)
 
+
 def get_gdrive_id(url):
     matches = re.findall(r"[-\w]{25,}", url)
     # We only expect one match from the URL.
     for m in matches:
         return m
+
 
 def trim_readme_name(name):
     """"Remove date in name for easier tracking in the repo."""
@@ -74,13 +76,16 @@ def trim_readme_name(name):
     else:
         return(name)
 
+
 def trim_data_file_name(name):
     """"Remove date in name for easier tracking in the repo."""
     return re.sub(r"(.*DOH COVID Data Drop_ \d{8} - )", r"", name)
 
+
 def trim_oddball_file_name(name):
     """"Remove date in name for easier tracking in the repo."""
     return re.sub(r"(.* \d{8} - )", r"", name)
+
 
 def download_gdrive_file(drive_service, file_id, download_path):
     request = drive_service.files().get_media(fileId=file_id)
@@ -91,6 +96,7 @@ def download_gdrive_file(drive_service, file_id, download_path):
     while done is False:
         status, done = downloader.next_chunk()
         logging.info("Downloading %d%%." % int(status.progress() * 100))
+
 
 def get_readme_id(drive_service):
     results = drive_service.files().list(
@@ -107,6 +113,7 @@ def get_readme_id(drive_service):
         return item['id']
     raise RemoteFileNotFoundError("DOH Readme Not Found")
 
+
 def list_data_files(drive_service, folder_id):
     results = drive_service.files().list(
                 q=f"parents in '{folder_id}' and trashed = false",
@@ -122,6 +129,7 @@ def list_data_files(drive_service, folder_id):
             yield item
         if results.get('nextPageToken', None) is None:
             break
+
 
 def download_data_files(drive_service, folder_id):
     if not os.path.exists(DATA_DIR):
@@ -146,6 +154,7 @@ def download_data_files(drive_service, folder_id):
             else:
                 raise e
 
+
 def extract_datadrop_link(filename):
     pdf = PyPDF2.PdfFileReader(filename)
     for page in range(pdf.numPages):
@@ -163,8 +172,10 @@ def extract_datadrop_link(filename):
                         return url
     raise PDFParsingError(f"Failed to extract datadrop link from {filename}")
 
+
 def get_full_url(url):
     return requests.head(url).headers['location']
+
 
 def get_datadrop_url(drive_service):
     readme_file_id = get_readme_id(drive_service)
@@ -172,6 +183,7 @@ def get_datadrop_url(drive_service):
     extracted_url = extract_datadrop_link(README_FILE_NAME)
     os.remove(README_FILE_NAME)
     return extracted_url
+
 
 def download(folder_id=None):
     drive_service = build_gdrive_service(CLIENT_KEY_PATH, TOKEN, ACCESS_SCOPES)
@@ -181,9 +193,11 @@ def download(folder_id=None):
         folder_id = get_gdrive_id(datadrop_full_url)
     download_data_files(drive_service, folder_id)
 
+
 def main():
     download()
     return 0
+
 
 if __name__ == "__main__":
     try:
